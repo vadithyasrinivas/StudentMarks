02bad0b0742f8e82dfc99e6d4b4c32191245746f
srinivasvadithya <vadithyasrinivas83@gmail.com>
2025-11-18 23:37:51 +0530

    Add project files.

---
+++ StudentMarks/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentMarks
+{
+    public class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+           Student student=new Student();
+            student.Name = "Srinivas";
+            student.Mark1 = 12;
+            student.Mark2 = 13;
+            student.Mark3 = 14;
+            int total=student.GetTotal();
+            //Console.WriteLine(total);
+            double avarege = student.GetAvarege();
+            //Console.WriteLine(avarege);
+        }
+    }
+}
---
+++ StudentMarks/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMarks
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Mark1 { get; set; }
+        public int Mark2 { get; set; }
+        public int Mark3 { get; set; }
+        public int GetTotal()
+        {
+            int TotalMarks = Mark1 + Mark2 + Mark3;
+            Console.WriteLine("Total Marks: " + TotalMarks);
+            return TotalMarks;
+        }
+        public double GetAvarege()
+        {
+            double Avarege = (Mark1 + Mark2 + Mark3) / 3;
+            Console.WriteLine("Avarege Marks: "+Avarege);
+            return Avarege;
+        }
+    }
+}
